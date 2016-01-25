4f43251dfe16e4ae5a44c8a0a6e8f80815818f8d
Howon Lee <howon@howonlee.com>
2016-01-25 13:58:51 -0800

    progress in pictures

--- pictures.py
+++ pictures.py
@@ -2,23 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def gradient_disp():
-    pass
+def gradient_disp(arr):
+    plt.close()
+    plt.savefig("pics/001_grad")
 
-def gradient_abs_disp():
-    pass
+def gradient_abs_disp(arr):
+    plt.close()
+    plt.savefig("pics/001_grad")
 
-def normalized_gradient_disp():
-    pass
+def normalized_gradient_disp(arr):
+    plt.close()
+    plt.savefig("pics/001_grad")
 
-def sampled_gradient_disp():
-    pass
+def sampled_gradient_disp(arr):
+    plt.close()
+    plt.savefig("pics/001_grad")
 
-def sampled_gradient_degree_disp():
-    pass
+def sampled_gradient_degree_disp(arr):
+    plt.close()
+    plt.savefig("pics/001_grad")
 
-def sample_gradient_otherstats():
-    pass
+def sample_gradient_otherstats(arr):
+    plt.close()
+    plt.savefig("pics/001_grad")
 
 if __name__ == "__main__":
-    pass
+    np_arr = np.load("grad_mat.npy")
+    print np_arr
