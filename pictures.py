645a9e21562e5dac505989c23d4fe766508fda4f
Howon Lee <howon@howonlee.com>
2016-01-25 13:38:52 -0800

    time to add pictures

---
+++ pictures.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def gradient_disp():
+    pass
+
+def gradient_abs_disp():
+    pass
+
+def normalized_gradient_disp():
+    pass
+
+def sampled_gradient_disp():
+    pass
+
+def sampled_gradient_degree_disp():
+    pass
+
+def sample_gradient_otherstats():
+    pass
+
+if __name__ == "__main__":
+    pass
