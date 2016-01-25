928eed5f44843481883caa7eeb11cb261a1a3128
Howon Lee <howon@howonlee.com>
2016-01-25 13:59:40 -0800

    outline all this stuff I did lots before

--- pictures.py
+++ pictures.py
@@ -8,23 +8,22 @@ def gradient_disp(arr):
 
 def gradient_abs_disp(arr):
     plt.close()
-    plt.savefig("pics/001_grad")
+    plt.savefig("pics/002_abs_grad")
 
 def normalized_gradient_disp(arr):
     plt.close()
-    plt.savefig("pics/001_grad")
+    plt.savefig("pics/003_normalized_grad")
 
 def sampled_gradient_disp(arr):
     plt.close()
-    plt.savefig("pics/001_grad")
+    plt.savefig("pics/004_sampled_grad")
 
 def sampled_gradient_degree_disp(arr):
     plt.close()
-    plt.savefig("pics/001_grad")
+    plt.savefig("pics/005_gradient_degrees")
 
 def sample_gradient_otherstats(arr):
-    plt.close()
-    plt.savefig("pics/001_grad")
+    print "other stats: "
 
 if __name__ == "__main__":
     np_arr = np.load("grad_mat.npy")
