897395c125a606d6f06348923444fefe19a6e337
Howon Lee <howon@howonlee.com>
2016-01-26 12:32:42 -0800

    fixed the diameter problem, hopefully

--- pictures.py
+++ pictures.py
@@ -58,19 +58,23 @@ def sampled_gradient_degree_disp(arr, title, filename):
     plt.gca().set_yscale("log")
     plt.savefig("pics/" + filename)
 
-# def sample_gradient_otherstats(arr):
-#     print "other stats: "
-#     new_arr = np.abs(arr)
-#     new_arr /= np.max(new_arr)
-#     new_arr = sample_arr(new_arr)
-#     net = nx.from_numpy_matrix(new_arr)
-#     print "diameter: ", nx.diameter(net)
-#     print "mean clustering coeffs", nx.clustering_coefficient(net)
+def sampled_gradient_otherstats(arr):
+    print "other stats: "
+    new_arr = np.abs(arr)
+    new_arr /= np.max(new_arr)
+    new_arr = sample_arr(new_arr)
+    arr_net = nx.from_numpy_matrix(new_arr[:-1, :])
+    giant_component_net = arr_net.subgraph(max(nx.connected_components(arr_net), key=len))
+    diameter = nx.diameter(giant_component_net)
+    clustering_coeff = nx.average_clustering(arr_net)
+    print "diameter: ", diameter
+    print "clustering coeff: ", clustering_coeff
 
 if __name__ == "__main__":
     np_arr = np.load("grad_mat.npy")
-    gradient_disp(np_arr, "Gradient", "a001_grad")
-    gradient_abs_disp(np_arr, "Absolute Gradient", "a002_abs_grad")
-    normalized_gradient_disp(np_arr, "Normalized Absolute Gradient", "a003_norm_grad")
-    sampled_gradient_disp(np_arr, "Sampled Gradient Network", "a004_grad_net")
-    sampled_gradient_degree_disp(np_arr, "Gradient Network Degree Distribution", "a005_grad_deg")
+    # gradient_disp(np_arr, "Gradient", "a001_grad")
+    # gradient_abs_disp(np_arr, "Absolute Gradient", "a002_abs_grad")
+    # normalized_gradient_disp(np_arr, "Normalized Absolute Gradient", "a003_norm_grad")
+    # sampled_gradient_disp(np_arr, "Sampled Gradient Network", "a004_grad_net")
+    # sampled_gradient_degree_disp(np_arr, "Gradient Network Degree Distribution", "a005_grad_deg")
+    sampled_gradient_otherstats(np_arr)
